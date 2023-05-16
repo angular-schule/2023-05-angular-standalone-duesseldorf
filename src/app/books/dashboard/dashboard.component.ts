9b065861eb9e4c598abe3f2320aed3973e03f0ab
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-16 13:34:48 +0200

    Signals

--- src/app/books/dashboard/dashboard.component.ts
+++ src/app/books/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { BookStoreService } from '../../shared/book-store.service';
 import { BookComponent } from '../book/book.component';
 import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-dashboard',
@@ -11,9 +12,20 @@ import { NgIf, NgFor, AsyncPipe } from '@angular/common';
     imports: [NgIf, NgFor, BookComponent, AsyncPipe]
 })
 export class DashboardComponent {
+
+  // name: string = 'fdhgjkhgfjdk';
+  name = signal('Ferdinand');
+  lastName = signal('Müller');
+  fullName = computed(() => this.name() + ' ' + this.lastName());
+
+  name$ = toObservable(this.name);
+
+
   // TS 2 // JS 1
   private bs = inject(BookStoreService);
   books$ = inject(BookStoreService).getAll();
+
+  books = toSignal(this.books$);
 
   // TS 1 // JS 2
   constructor(
@@ -21,6 +33,16 @@ export class DashboardComponent {
   ) {
     // TS 3 // JS 3
     console.log('DB');
+  }
+
+
+
+  updateName(name: string) {
+    this.name.set(name);
+  }
+
+  updateLastName(name: string) {
+    this.lastName.set(name);
   }
 }
 
