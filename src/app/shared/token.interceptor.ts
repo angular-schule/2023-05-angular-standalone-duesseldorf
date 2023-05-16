2afe87e4c2e7f46616941dd6ec0e19a06a5a7cbb
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-16 12:33:44 +0200

    Functional Interceptors

--- src/app/shared/token.interceptor.ts
+++ src/app/shared/token.interceptor.ts
@@ -1,25 +1,14 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
-@Injectable()
-export class TokenInterceptor implements HttpInterceptor {
+export const tokenInterceptor: HttpInterceptorFn = (request, next) => {
+  const clonedRequest = request.clone({
+    setHeaders: {
+      Authorization: 'Bearer ' + inject(AuthService).getAuthToken()
+    }
+  })
 
-  constructor(private authService: AuthService) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const clonedRequest = request.clone({
-      setHeaders: {
-        Authorization: 'Bearer ' + this.authService.getAuthToken()
-      }
-    })
-
-    return next.handle(clonedRequest);
-  }
-}
+  return next(clonedRequest);
+};
--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,24 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { TokenInterceptor } from './app/shared/token.interceptor';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { adminRoutes } from './app/admin/admin.routes';
 import { myApiUrl } from './token';
+import { tokenInterceptor } from './app/shared/token.interceptor';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-      {
-          provide: HTTP_INTERCEPTORS,
-          useClass: TokenInterceptor,
-          multi: true
-      },
-      provideHttpClient(withInterceptorsFromDi()),
-      provideRouter([...routes, ...adminRoutes]),
-      { provide: myApiUrl, useValue: 'https://api.angular.schule' }
+    provideHttpClient(
+      withInterceptors([tokenInterceptor])
+    ),
+    provideRouter([...routes, ...adminRoutes]),
+
+
+
+
+    { provide: myApiUrl, useValue: 'https://api.angular.schule' }
   ]
 })
   .catch(err => console.error(err));
-
-
-  // Inhalte von Modulen
-  // Komponente, Direktiven, Pipes
-  // Providers
-  // (andere Module)
