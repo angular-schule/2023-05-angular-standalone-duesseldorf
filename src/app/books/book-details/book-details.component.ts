54858180062adf37d73d137aae876a0b4ae256ba
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-16 12:50:36 +0200

    takeUntil: Subscription beenden

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, Injector, Input, inject, runInInjectionContext } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { map, switchMap } from 'rxjs';
+import { Subject, map, switchMap, takeUntil } from 'rxjs';
 
 import { BookStoreService } from '../../shared/book-store.service';
 import { NgIf, AsyncPipe } from '@angular/common';
@@ -16,10 +16,21 @@ export class BookDetailsComponent {
 
   @Input() isbn?: string;
 
+  private destroy$ = new Subject<void>()
+
   private bs = inject(BookStoreService);
 
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(params => params.get('isbn')!),
-    switchMap(isbn => this.bs.getSingle(isbn))
+    switchMap(isbn => this.bs.getSingle(isbn)),
+    takeUntil(this.destroy$)
   );
+
+  constructor() {
+    this.book$.subscribe(e => console.log(e));
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
 }
